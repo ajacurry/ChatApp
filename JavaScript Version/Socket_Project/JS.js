1606350b7326f2f97a354e85e03a967245170e36
Kali <kapdayalexis@gmail.com>
2024-04-26 15:31:43 -0400

    UI

--- JavaScript Version/Socket_Project/JS.js
+++ JavaScript Version/Socket_Project/JS.js
@@ -1,16 +1,20 @@
+
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-   res.sendFile('JSUI.html', {root: '.'});});
+   res.sendFile('JSUI.html', {root: '.'});
+});
+   
 users = [];
 io.on('connection', function(socket){
-   console.log('A user connected');
+   console.log('A user is connecting');
    socket.on('setUsername', function(data){
       console.log(data);
       if(users.indexOf(data) > -1){
-         socket.emit('userExists', data + ' username is taken! Try some other username.');
+         socket.emit('userExists', data + ' Username already in use.');
       } else {
          users.push(data);
          socket.emit('userSet', {username: data});
@@ -21,6 +25,18 @@ io.on('connection', function(socket){
       io.sockets.emit('newmsg', data);
    })
 });
+io.on("connection", (socket) => {
+   socket.on("upload", (file, callback) => {
+     console.log(file); // <Buffer 25 50 44 ...>
+ 
+     // save the content to the disk, for example
+     writeFile("/tmp/upload", file, (err) => {
+       callback({ message: err ? "failure" : "success" });
+     });
+   });
+ });
+
+
 http.listen(3000, function(){
    console.log('listening on localhost:3000');
 });
